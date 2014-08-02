8b7782141a66d2074131650329bc151120086610
Josh Guffin <josh.guffin@gmail.com>
2014-08-02 15:46:42 -0400

    Create setup framework

---
+++ setup.py
@@ -0,0 +1,17 @@
+from reviewboard.extensions.packaging import setup
+
+GITHUB_URL = 'https://github.com/joshguffin/rboard_bugzilla'
+VERSION = "0.1"
+PACKAGE = 'rboard_bugzilla'
+EXTENSION = '{0} = {0}.extension:BugzillaExtension'.format(PACKAGE),
+
+setup(
+    name=PACKAGE,
+    version=VERSION,
+    description='Post to BZs',
+    url=GITHUB_URL,
+    author='Josh Guffin',
+    author_email='josh.guffin@gmail.com',
+    packages=[PACKAGE],
+    entry_points={'reviewboard.extensions': [EXTENSION]}
+)
