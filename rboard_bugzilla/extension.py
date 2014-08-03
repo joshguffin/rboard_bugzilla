d4390ecbf50128846dc9b5b0d1df8aa9f3a58aa0
Josh Guffin <josh.guffin@gmail.com>
2014-08-02 20:05:50 -0400

    Refactor

--- rboard_bugzilla/extension.py
+++ rboard_bugzilla/extension.py
@@ -9,6 +9,37 @@ from bzlib import bugzilla
 
 MESSAGE = "A code review associated to this bug has been created at {0}"
 
+def _extension_applies(review_request):
+    if not review_request:
+        return False
+
+    bugs = review_request.get_bug_list()
+    if not bugs:
+        return False
+
+    tracker_url = review_request.repository.bug_tracker
+    return tracker_url
+
+
+def _get_added_bug_list(review_request):
+    '''
+    Recover bug numbers that have been added during this publication
+    '''
+    changes = review_request.changedescs
+    try:
+        latest_change = changes.filter(public=True).latest()
+    except ChangeDescription.DoesNotExist:
+        # this is the initial publication of the review_request
+        return review_request.get_bug_list()
+
+    fields = latest_change.fields_changed
+    bug_changes = fields.get('bugs_closed', None)
+    if not bug_changes:
+        return []
+
+    added = bug_changes.get('added', None)
+    return [bug for sublist in added for bug in sublist]
+
 
 class BugzillaExtension(Extension):
 
@@ -19,56 +50,30 @@ class BugzillaExtension(Extension):
         'Summary': 'posts to bz whenever the bz field changes',
     }
 
+
     def initialize(self):
+        '''
+        Register to be notified when a review request is published
+        '''
         SignalHook(self, review_request_published, self.on_published)
+
 
     def on_published(self, review_request=None, **kwargs):
         '''
         Handle signal; post to bugzilla for any new bug dependencies
         '''
-        if not review_request:
+        if not _extension_applies(review_request):
             return
 
-        bugs = review_request.get_bug_list()
-        if not bugs:
-            return
-
-        tracker_url = review_request.repository.bug_tracker
-        if not tracker_url:
-            return
-
-        config = self.settings
-        setting_names = ['site_url', 'username', 'password']
-        for name in setting_names:
-            if config.get(name):
-                continue
-            this = self.__class__.__name__
-            logging.error("{0}: missing config setting '{0}'".format(this, name))
-            return
-
-        changes = review_request.changedescs
-        try:
-            latest_change = changes.filter(public=True).latest()
-        except ChangeDescription.DoesNotExist:
-            latest_change = None
-
-        # this is the initial publication of the review_request
-        if not latest_change:
+        bugs = _get_added_bug_list(review_request)
+        if bugs:
             self._post_bugs(review_request, bugs)
-            return
-
-        fields = latest_change.fields_changed
-        bug_changes = fields.get('bugs_closed', None)
-        if not bug_changes:
-            return
-
-        added = bug_changes.get('added', None)
-        added = [bug for sublist in added for bug in sublist]
-        if added:
-            self._post_bugs(review_request, added)
 
 
     def _post_bugs(self, review_request, bugs):
+        '''
+        Post updates to bugs that we haven't already posted to
+        '''
         key = 'rboard_bugzilla_posted_bugs'
         url = review_request.get_absolute_url()
 
@@ -80,8 +85,9 @@ class BugzillaExtension(Extension):
                 continue
 
             try:
-                self._post_bug(url, bug)
+                self._post_bug_to_bugzilla(url, bug)
             except Exception as error:
+                # unfortunately bugzillatools sometimes raises raw Exceptions
                 msg = "{0}: Failed posting bug {1} for review {2}: {3}"
                 this = self.__class__.__name__
                 logging.error(msg.format(this, bug, review_request.id, error))
@@ -89,20 +95,21 @@ class BugzillaExtension(Extension):
 
             posted_bugs.append(bug)
 
-        review_request.extra_data[key] = ",".join([bug for bug in posted_bugs])
+        review_request.extra_data[key] = ",".join(posted_bugs)
         review_request.save()
 
 
-    def _post_bug(self, url, bug):
+    def _post_bug_to_bugzilla(self, url, bug):
+        '''
+        Post data to bugzilla
+        '''
         settings = self.settings
         base_url = settings.get('site_url')
         username = settings.get('username')
         password = settings.get('password')
 
-        message = MESSAGE.format(url)
-
         bz_api = bugzilla.Bugzilla(base_url, username, password)
         bz_bug = bz_api.bug(bug)
 
-        bz_bug.add_comment(message)
+        bz_bug.add_comment(MESSAGE.format(url))
         bz_bug.update()
